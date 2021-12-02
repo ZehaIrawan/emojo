f5810d680ce54df28b2ab4adcf803ffed3da3667
Zeha Irawan <zehairawan@gmail.com>
2021-12-02 23:18:32 +0800

    test: url

---
+++ src/component/Comb1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Comb1 = () => {
+  return (
+    <div>
+      <h1>This is 😎🎵😎</h1>
+      😎🎵😎
+    </div>
+  );
+}
+
+export default Comb1
---
+++ src/component/Comb2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Comb2 = () => {
+  return <div>This is 🚀🚀🚀</div>;
+}
+
+export default Comb2
